964e4872545869822c98f855344a25e29882a54d
Marco Lussetti <marco@marcolussetti.com>
2020-11-06 18:58:32 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# outages.repogenerator
+
+This tool is part of the [Outages Project](https://github.com/outages). It asks the user a series of question and then
+generates a simple scaffolding for a tracking outages in the Outages Project format.
+
+## Install
+
+```bash
+pip install outages.repogenetator
+```
+
+## Execution
+
+```bash
+outages-repogenerator
+```
+
+Answer prompts as asked.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="outages.repogenerator",
+    version="0.0.1",
+    author="Marco Lussetti and the Outages Project",
+    author_email="packages@marcolussetti.com",
+    description="Generate a standard Outages Project repository.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/outages/repogenerator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    scripts=[
+        'bin/outages-repogenerator'
+    ],
+    install_requires=[
+    ]
+)
